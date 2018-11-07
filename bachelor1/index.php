aaaab6b85916d5755b6a6a9bac116d84e16a2c29
mina12 <amina12ben@gmail.com>
2018-11-07 15:29:51 +0100

    done

---
+++ bachelor1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p> modification </p>
+    <p> essai hoby2</p>
+    <p> commit encore une fois </p>
+    <p>Toutes les fables de Jean de la Fontaine dans leur intégralité.</p>
+
+  </body>
+</html>
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <title></title>
   </head>
   <body>
-    <p>bonjour à tous</p>
 
   </body>
 </html>
